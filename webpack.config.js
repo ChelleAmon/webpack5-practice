0a31b213afeb3648f2aa586be2d722170e368847
Gichelle Amon <amongichelle@gmail.com>
2022-06-07 13:51:41 -0700

    Instantiate CleanWebpackPlugin into the plugins array

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './dist'), //outputPath folder
         publicPath: 'dist/'
         // publicPath: 'http://some-cdn.com'
     },
@@ -58,6 +59,7 @@ module.exports = {
         new TerserPlugin(),
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css',
-        })
+        }),
+        new CleanWebpackPlugin(),
     ]
 }
