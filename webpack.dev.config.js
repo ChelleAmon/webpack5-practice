d85f07f588d6e8349e67e5bf8bb4dfc489447b24
Gichelle Amon <amongichelle@gmail.com>
2022-06-07 16:39:39 -0700

    create webpack config for development mode, errors after running npm run dev

---
+++ webpack.dev.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'), //output.path folder
+        publicPath: ''
+        // publicPath: 'http://some-cdn.com'
+    },
+    mode: 'development', 
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|jpeg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize:  3 * 1024 // 3 kilobytes
+                    } // condition based on which webpage decides if it should use asset inline or as a resource
+                } // accepts a JS object as a value
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source'
+            },// will read the text file as a javascript string with those contents
+            {
+                test: /\.css$/,
+                use: [
+                    'style.loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style.loader', 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env' ], // converts ECMASCRIPT to its older version
+                        plugins: [ '@babel/plugin-proposal-class-properties' ] // 
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [ //removes old files before Webpack generates the new files
+                '**/*', //means remove all the files together with subdirectories inside the output.path.folder
+                path.join(process.cwd(), 'build/**/*') // removes all files inside the build folder
+            ]
+        }),
+        new HtmlWebpackPlugin({ //pass additional options, e.g. specify a custom title. You can create your own html template while customizing the options from this plugin
+            title: "Hello World 2",
+            template: 'src/index.hbs',
+            // filename: 'subfolder/custom_filename.html', // customize subfolder and its customized html name
+            description: 'I am testing with another description'
+        }),
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css'],
+        modules: ['node_modules']      
+    }
+}
