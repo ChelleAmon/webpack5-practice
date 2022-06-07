cc463f1ee9690923520fd9bc6c7be328bb399a80
Gichelle Amon <amongichelle@gmail.com>
2022-06-07 09:39:27 -0700

    add new rule, add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,12 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
             }
         ]
     }
