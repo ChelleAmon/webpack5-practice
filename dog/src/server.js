9dd37592f33a3c7ba03bb6e17896f168ea305abe
Gichelle Amon <amongichelle@gmail.com>
2022-06-10 13:50:13 -0700

    `npm run dev`

--- dog/src/server.js
+++ dog/src/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const clientPath = path.resolve(__dirname, '../dist')
 
-app.use(express.static(clientPath))
+app.use('/static', express.static(clientPath))
 
 app.get('/', (req,res) => {
     const pathToHtmlFile = path.resolve(__dirname, '../dist/dog.html');
