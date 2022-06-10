3d604687d8b8680f70ad8c6a54a7072ba0b3482e
Gichelle Amon <amongichelle@gmail.com>
2022-06-09 20:14:01 -0700

    change path for the static route

--- hello-world/src/dog.js
+++
@@ -1,8 +0,0 @@
-import Heading from "./components/heading/heading.js";
-import DogImage from './components/dog-image/dog-image.js';
-
-const heading = new Heading();
-heading.render('Dog');
-
-const dogImage = new DogImage();
-dogImage.render()
