67f779d219d42f1defbc063910ffae0b8b486087
Gichelle Amon <amongichelle@gmail.com>
2022-06-10 14:51:58 -0700

    create new js file, import applications

--- dashboard/src/dashboard.js
+++ dashboard/src/dashboard.js
@@ -1,1 +1,23 @@
+//Get Url
+const url = window.location.pathname // taking part of url and it does not contain the domain and port
+
+
+//Import remote modules dynamically by creating asynchronous function
+
+if(url === '/hello-world-page'){
+    import ('HelloWorldApp/HelloWorldPage').then(HelloWorldPageModule => { 
+        const HelloWorldPage = HelloWorldPageModule.default;
+        const helloWorldPage = new HelloWorldPage();
+        helloWorldPage.render()
+    });
+}else if (url === '/dog-page'){
+    import ('DogPage/DogPage').then(DogPageModule => {
+        const DogPage = DogPageModule.default;
+        const dogPage = new DogPage();
+        dogPage.render();
+    })
+}
+
+
+
 console.log('dashboard');
